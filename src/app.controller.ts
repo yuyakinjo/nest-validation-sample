ac8f2065a3afc90e2b9387311faeca6baba0cb2f
kinjo yuya <kinjo.bobchin@gmail.com>
2022-02-02 16:31:14 +0900

    chore: GET, POSTの場合を用意

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Param, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { GreetingDto } from './greeting-dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get(':id')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  getHello(@Query() queries: GreetingDto, @Param() params: GreetingDto) {
+    return { queries, params };
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  getGreeting(@Body() greeting: GreetingDto) {
+    return greeting;
   }
 }
